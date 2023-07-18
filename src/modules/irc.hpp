1b1e13828bb7011efd81e816f6b18246e0db2015
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-07-18 05:28:15 +0100

    <update>: Header files

--- src/modules/irc.hpp
+++ src/modules/irc.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 22:37:16 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/07/18 01:13:56 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/07/18 02:35:57 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 #ifndef __IRC_HPP__
 # define __IRC_HPP__
 
-# include <iostream>
 # include "macros.hpp"
-
-typedef std::string	str_t;
 
 void	panic(const char *msg);
 bool	is_valid_password(const char *pass);
--- src/modules/macros.hpp
+++ src/modules/macros.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 00:50:07 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/07/18 00:51:09 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/07/18 02:36:04 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 #ifndef __MACROS_HPP__
 # define __MACROS_HPP__
 
+# include <iostream>
+
 # define COLOR_RED "\033[0;31m"
 # define COLOR_DEFAULT "\033[0;37m"
 
+typedef std::string	str_t;
 
 #endif
