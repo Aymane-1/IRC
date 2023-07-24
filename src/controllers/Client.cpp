18d3ac8c4f91c6f81872244d19678d4760ccc90d
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-07-24 01:47:14 +0100

    <add>: Client to the archeticture

--- src/controllers/Client.cpp
+++ src/controllers/Client.cpp
@@ -1,34 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Client.hpp                                         :+:      :+:    :+:   */
+/*   Client.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/17 04:04:05 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/07/18 03:35:28 by sel-kham         ###   ########.fr       */
+/*   Created: 2023/07/18 02:41:57 by sel-kham          #+#    #+#             */
+/*   Updated: 2023/07/18 02:47:27 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-#ifndef __CLIENT_HPP__
-# define __CLIENT_HPP__
+#include "../modules/Client.hpp"
 
-# include "macros.hpp"
-
-class Client
-{
-private:
-	enum	grade {OPERATOR, USER};
-	str_t	nickname;
-	str_t	hostname;
-	int		grade;
-	/* data */
-public:
-	Client(/* args */);
-	~Client();
-	static const str_t forbiddenChars;
-	static const str_t forbiddenToStartWith;
-};
-
-#endif
+const str_t	Client::forbiddenChars = " ,*?!@.#&";
+const str_t	Client::forbiddenToStartWith = "#&$:";
