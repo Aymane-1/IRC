cd2885214c8293de45b01562e2948d823843e1a7
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-07-29 02:28:56 +0100

    Establish Client class

--- src/controllers/Client.cpp
+++ src/controllers/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 02:41:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/07/18 02:47:27 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/07/29 02:27:57 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,62 @@
 
 const str_t	Client::forbiddenChars = " ,*?!@.#&";
 const str_t	Client::forbiddenToStartWith = "#&$:";
+unsigned long	Client::count = 0;
+ 
+ /* Constructors & Distructors */
+ Client::Client(const unsigned int &fd)
+ {
+	this->clientFd = fd;
+	this->id = Client::count++;
+ }
+
+ Client::~Client(void) { }
+
+ /* Getters & Setters */
+ const unsigned int	&Client::getId(void) const
+ {
+	return (this->id);
+ }
+
+ const unsigned long	&Client::getClientFd(void) const
+ {
+	return (this->clientFd);
+ }
+
+ const str_t			&Client::getNickname(void) const
+ {
+	return (this->nickname);
+ }
+
+ const str_t			&Client::getUsername(void) const
+ {
+	return (this->username);
+ }
+
+void	Client::setId(const unsigned int &id)
+{
+	this->id = id;
+}
+
+void	Client::setClientFd(const long int &fd)
+{
+	this->clientFd = fd;
+}
+
+void	Client::setNickname(const str_t &nickname)
+{
+	int	i;
+
+	i = -1;
+	if (!nickname[0] || Client::forbiddenToStartWith.find(nickname[0]) < Client::forbiddenToStartWith.length())
+		throw (std::runtime_error("Invalid nickname."));
+	while (nickname[++i])
+		if (Client::forbiddenChars.find(nickname[i]) < Client::forbiddenChars.length())
+			throw (std::runtime_error("Invalid nickname."));
+	this->nickname = nickname;
+}
+
+void	Client::setUsername(const str_t &username)
+{
+	this->username = username;
+}
--- src/modules/Client.hpp
+++ src/modules/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 04:04:05 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/07/18 03:35:28 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/07/29 02:07:01 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,28 @@
 class Client
 {
 private:
-	enum	grade {OPERATOR, USER};
-	str_t	nickname;
-	str_t	hostname;
-	int		grade;
-	/* data */
+	unsigned int	id;
+	unsigned long	clientFd;
+	str_t			nickname;
+	str_t			username;
 public:
-	Client(/* args */);
-	~Client();
+	/* Constructors & Distructors */
+	Client(const unsigned int &fd);
+	~Client(void);
+	/* Class data */
 	static const str_t forbiddenChars;
 	static const str_t forbiddenToStartWith;
+	static unsigned long	count;
+	/* Getters & Setters */
+	const unsigned int	&getId(void) const;
+	const unsigned long	&getClientFd(void) const;
+	const str_t			&getNickname(void) const;
+	const str_t			&getUsername(void) const;
+	
+	void	setId(const unsigned int &id);
+	void	setClientFd(const long int &fd);
+	void	setNickname(const str_t &nickname);
+	void	setUsername(const str_t &username);
 };
 
 #endif
