5245a9d0d6018ca4581a9137bc42c20e80e5d13c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-08-02 00:06:00 +0100

    <update>: Server class

--- src/controllers/Server.cpp
+++ src/controllers/Server.cpp
@@ -6,16 +6,27 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 06:02:02 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/08/01 20:59:01 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/08/02 00:01:45 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../modules/Server.hpp"
 
 // Constructors & Distructors
+Server::Server(void) : port(-1), password("") { }
+
 Server::Server(int port, str_t password) : port(port), password(password) { }
 
 Server::~Server(void) { }
+
+Server			&Server::operator=(const Server &other)
+{
+	this->port = other.port;
+	this->password = other.password;
+	this->request = other.request;
+	this->socketFd = other.socketFd;
+	return (*this);
+}
 
 // Geters & Setters
 const int	&Server::getPort(void) const
--- src/modules/Server.hpp
+++ src/modules/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 04:01:30 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/08/01 20:56:18 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/08/01 23:56:13 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,35 +19,29 @@
 # include <netinet/in.h>
 # include <unistd.h>
 # include <poll.h>
-# include <map>
 # include "macros.hpp"
 # include "Client.hpp"
-
-typedef std::map<const str_t, Client> client_m;
 
 class Server
 {
 private:
-	const int			port;
-	const str_t			password;
-	str_t				request;
-	int					socketFd;
-	struct pollfd		fds;
+	int		port;
+	str_t	password;
+	str_t	request;
+	int		socketFd;
+	// struct pollfd		fds;
 public:
+	Server(void);
 	Server(int port, str_t password);
 	~Server(void);
 	// Getters and Setters
+	Server			&operator=(const Server &other);
 	const int		&getPort(void) const;
 	const str_t		&getPassword(void) const;
 	const int		&getSocketFd(void) const;
 	const str_t		&getRequest(void) const;
-	const client_m	&getClients(void) const;
 	void			setRequest(const str_t &req);
 	void			setSocketFd(const int fd);
-	void			setClients(const client_m &clients);
-	// Class functionallities
-	int				init(void);
-	int				run(void);
 };
 
 #endif
