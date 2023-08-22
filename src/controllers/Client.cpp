6c1a587bcf9ab29fb14e71dc506816ec3c608c7f
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-08-22 20:24:52 +0100

    <add>: client class

--- src/controllers/Client.cpp
+++ src/controllers/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 02:41:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/08/01 20:58:40 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/08/18 20:15:44 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,38 @@
 const str_t	Client::forbiddenChars = " ,*?!@.#&";
 const str_t	Client::forbiddenToStartWith = "#&$:";
  
- /* Constructors & Distructors */
- Client::Client(const unsigned int &fd)
- {
-	this->clientFd = fd;
- }
+/* Constructors & Distructors */
+Client::Client(void) { }
 
- Client::~Client(void) { }
+Client::~Client(void) { }
 
- /* Getters & Setters */
- const unsigned long	&Client::getClientFd(void) const
- {
+/* Getters & Setters */
+const int	&Client::getClientFd(void) const
+{
 	return (this->clientFd);
- }
+}
 
- const str_t			&Client::getNickname(void) const
- {
-	return (this->nickname);
- }
+const str_t			&Client::getNickname(void) const
+{
+return (this->nickname);
+}
 
- const str_t			&Client::getUsername(void) const
- {
-	return (this->username);
- }
+const str_t			&Client::getUsername(void) const
+{
+return (this->username);
+}
 
-void	Client::setClientFd(const long int &fd)
+const bool	&Client::getIsAuthentecated(void) const
+{
+	return (this->isAuthentecated);
+}
+
+const bool	&Client::getIsMod(void) const
+{
+	return (this->isMod);
+}
+
+void	Client::setClientFd(const int &fd)
 {
 	this->clientFd = fd;
 }
@@ -60,4 +67,14 @@ void	Client::setNickname(const str_t &nickname)
 void	Client::setUsername(const str_t &username)
 {
 	this->username = username;
+}
+
+void	Client::setIsAuthentecated(const bool &isAuth)
+{
+	this->isAuthentecated = isAuth;
+}
+
+void	Client::setIsMod(const bool &isMod)
+{
+	this->isMod = isMod;
 }
--- src/modules/Client.hpp
+++ src/modules/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 04:04:05 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/08/01 20:57:41 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/08/18 20:15:35 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,30 @@
 class Client
 {
 private:
-	unsigned long	clientFd;
-	str_t			nickname;
-	str_t			username;
-	int				socketFd;
+	int		clientFd;
+	str_t	nickname;
+	str_t	username;
+	bool	isAuthentecated;
+	bool	isMod;
 public:
 	/* Constructors & Distructors */
-	Client(const unsigned int &fd);
+	Client(void);
 	~Client(void);
 	/* Class data */
 	static const str_t forbiddenChars;
 	static const str_t forbiddenToStartWith;
-	static unsigned long	count;
 	/* Getters & Setters */
-	const unsigned long	&getClientFd(void) const;
-	const str_t			&getNickname(void) const;
-	const str_t			&getUsername(void) const;
-	const str_t			&getSocketFd(void) const;
+	const int	&getClientFd(void) const;
+	const str_t	&getNickname(void) const;
+	const str_t	&getUsername(void) const;
+	const bool	&getIsAuthentecated(void) const;
+	const bool	&getIsMod(void) const;
 	
-	void	setClientFd(const long int &fd);
+	void	setClientFd(const int &fd);
 	void	setNickname(const str_t &nickname);
 	void	setUsername(const str_t &username);
-	void	setSocketFd(const str_t &socketFd);
+	void	setIsAuthentecated(const bool &isAuth);
+	void	setIsMod(const bool &isMod);
 };
 
 #endif
