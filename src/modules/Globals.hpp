f90838b1c45a0e78ee013fa03a35d99f268a192c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-09-03 21:34:29 +0100

    <edit>: Error replies

--- src/modules/Globals.hpp
+++ src/modules/Globals.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 01:42:30 by sel-kham          #+#    #+#             */
-/*   Updated: 2023/09/03 03:42:51 by sel-kham         ###   ########.fr       */
+/*   Updated: 2023/09/03 21:33:20 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,16 @@
 typedef std::string	str_t;
 
 /* NUMERIC REPLIES */
-# define RPL_WELCOME(server, nick) ":" + server + " 001 " + nick + " :Welcome to a7sn IRC server\r\n"
+# define RPL_WELCOME(server, nick) ":" + server + " 001 " + nick + " :Welcome to a7sn IRC server," + nick + "\r\n"
 
 /* ERROR REPLIES */
-# define ERR_NEEDMOREPARAMS(server, nickname) ":" + server  + " " + "461" + " " + nickname + ": Need more parameters.\r\n"
-# define ERR_PASSWDMISMATCH(server, nickname) ":" + server  + " " + "464" + " " + nickname + ": Invalid password.\r\n"
-# define ERR_ALREADYREGISTERED(server, nickname) ":" + server  + " " + "462" + " " + nickname + ": Already authenticated.\r\n"
-# define ERR_NONICKNAMEGIVEN(server, nickname) ":" + server  + " " + "431" + " " + nickname + ": No nickname provided.\r\n"
-# define ERR_ERRONEUSNICKNAME(server, nickname) ":" + server  + " " + "432" + " " + nickname + ": Invalid nickname.\r\n"
-# define ERR_NICKNAMEINUSE(server, nickname) ":" + server  + " " + "433" + " " + nickname + ": Nickname already in use.\r\n"
-# define ERR_NOTREGISTERED(server, nickname) ":" + server + " " + "451" + " " + nickname + ": You have not registered.\r\n"
+# define ERR_NEEDMOREPARAMS(server, nickname) ":" + server  + " 461 " + nickname + ": Need more parameters.\r\n"
+# define ERR_PASSWDMISMATCH(server, nickname) ":" + server  + " 464 " + nickname + ": Invalid password.\r\n"
+# define ERR_ALREADYREGISTERED(server, nickname) ":" + server  + " 462 " + nickname + ": Already authenticated.\r\n"
+# define ERR_NONICKNAMEGIVEN(server, nickname) ":" + server  + " 431 " + nickname + ": No nickname provided.\r\n"
+# define ERR_ERRONEUSNICKNAME(server, nickname) ":" + server  + " 432 " + nickname + ": Invalid nickname.\r\n"
+# define ERR_NICKNAMEINUSE(server, nickname) ":" + server  + " 433 " + nickname + ": Nickname already in use.\r\n"
+# define ERR_NOTREGISTERED(server, nickname) ":" + server + " 451 " + nickname + ": You have not registered.\r\n"
 
 namespace Helpers
 {
