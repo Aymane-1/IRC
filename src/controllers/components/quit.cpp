e4085c01544da06e6d2c3dbc84895698e11cc12f
aechafii <echafii.aymane@gmail.com>
2023-09-13 06:12:12 +0100

    -fix: close connection from server

--- src/controllers/components/quit.cpp
+++ src/controllers/components/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 20:32:15 by aechafii          #+#    #+#             */
-/*   Updated: 2023/09/07 20:21:30 by aechafii         ###   ########.fr       */
+/*   Updated: 2023/09/13 03:31:56 by aechafii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 str_t	CommandWorker::quit(Client &client)
 {
 	str_t	reason = "";
+	str_t	serverResponse = "";
 	size_t	index = 0;
 	index = this->request.find_first_of(" ");
 	if (index != str_t::npos)
-		reason = this->request.substr(index, this->request.length());
-	str_t	serverResponse = RPL_CLSLINK;
+		reason = this->request.substr(index + 1, this->request.length());
+	if (reason.empty())
+		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Client exited");
+	else
+		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Quit: " + reason);
 	send(client.getSocketFd(), serverResponse.c_str(), serverResponse.length(), 0);
 	this->server->clean(client.getSocketFd());
-	return (RPL_QUIT(client.getNickname(), this->server->getHost(), reason));
+	return ("");
 }
