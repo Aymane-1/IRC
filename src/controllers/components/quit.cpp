2ecdfe05234d5da13b3f00b263d3f1401d5418fc
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-09-15 04:37:14 +0100

    ~Update: QUIT

--- src/controllers/components/quit.cpp
+++ src/controllers/components/quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 20:32:15 by aechafii          #+#    #+#             */
-/*   Updated: 2023/09/14 00:35:25 by aechafii         ###   ########.fr       */
+/*   Updated: 2023/09/15 04:36:42 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,20 @@ str_t	CommandWorker::quit(Client &client)
 	str_t	reason = "";
 	str_t	serverResponse = "";
 	size_t	index = 0;
+	
 	index = this->request.find_first_of(" ");
 	if (index != str_t::npos)
-		reason = this->request.substr(index + 1, this->request.length());
+		reason = this->request.substr(index, this->request.length());
 	if (reason.empty())
 		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Client exited");
 	else
+	{
+		reason = Helpers::trim(reason, ' ');
+		if (reason[0] == ':')
+			reason = reason.substr(1, reason.length());
 		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Quit: " + reason);
+	}
 	send(client.getSocketFd(), serverResponse.c_str(), serverResponse.length(), 0);
-	return ("QUIT");
+	this->server->clean(client.getSocketFd());
+	return ("");
 }
