47f2d6158ea8388c3c626a55be4e5796f38451db
7ach7ouch_101 <mahmoud_meziani@outlook.sa>
2023-09-15 10:36:36 +0100

    quit

---
+++ src/components/quit.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quit.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 20:32:15 by aechafii          #+#    #+#             */
+/*   Updated: 2023/09/15 04:36:42 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../modules/CommandWorker.hpp"
+
+str_t	CommandWorker::quit(Client &client)
+{
+	str_t	reason = "";
+	str_t	serverResponse = "";
+	size_t	index = 0;
+	
+	index = this->request.find_first_of(" ");
+	if (index != str_t::npos)
+		reason = this->request.substr(index, this->request.length());
+	if (reason.empty())
+		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Client exited");
+	else
+	{
+		reason = Helpers::trim(reason, ' ');
+		if (reason[0] == ':')
+			reason = reason.substr(1, reason.length());
+		serverResponse = RPL_QUIT(RPL_CLSLINK, client.getNickname(), this->server->getHost(), "Quit: " + reason);
+	}
+	send(client.getSocketFd(), serverResponse.c_str(), serverResponse.length(), 0);
+	this->server->clean(client.getSocketFd());
+	return ("");
+}
