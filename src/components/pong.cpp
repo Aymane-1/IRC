84d866e781578fcc8c294e4827613797fd0c158b
7ach7ouch_101 <mahmoud_meziani@outlook.sa>
2023-09-15 10:36:27 +0100

    pong

---
+++ src/components/pong.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pong.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 06:17:59 by sel-kham          #+#    #+#             */
+/*   Updated: 2023/09/11 02:05:23 by aechafii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../modules/CommandWorker.hpp"
+
+str_t	CommandWorker::pong(Client &client)
+{
+	(void)client;
+// 	size_t	i;
+
+// 	if (client.getVAuth() != FULL_AUTH)
+// 		return (ERR_NOTREGISTERED(this->server->getHost(), client.getNickname()));
+// 	i = this->request.find_first_of(" ");
+// 	if (i == str_t::npos)
+// 		return ("ERR_NOORIGIN(this->server->getHost(), client.getNickname())");
+	return ("");
+}
